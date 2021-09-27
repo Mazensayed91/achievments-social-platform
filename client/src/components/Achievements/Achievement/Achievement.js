176d62d9c0f83e23dd96d2cce831fe3bda00bc20
Mazen Sayed <54520113+Mazensayed91@users.noreply.github.com>
2021-09-27 14:40:19 +0200

    almost finished Achievement, Achievements Components

--- client/src/components/Achievements/Achievement/Achievement.js
+++ client/src/components/Achievements/Achievement/Achievement.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import moment from 'moment';
+import {Card, CardActions, CardContent, CardMedia, Button, Typography} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt"
+import DeleteIcon from "@material-ui/icons/Delete"
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import useStyles from "./styles"
 
-const Achievement = () => {
+const Achievement = ({ achievement }) => {
+    const classes = useStyles()
     return (
-        <>
-            <h1>Hello</h1>
-        </>
+        <Card className={classes.card}>
+            {console.log("achie", achievement)}
+            <CardMedia className={classes.media} image={achievement.selectedFile} title={achievement.title}/>
+            <div className={classes.overlay}>
+                <Typography varient="h6">
+                    {achievement.creator}
+                </Typography>
+                <Typography varient="h6">
+                    {moment(achievement.createdAt).fromNow()}
+                </Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color:'white'}} size="small" onClick={() => {}}>
+                    <MoreHorizIcon fontSize="medium"/>
+                </Button>
+            </div>
+            <div>
+                <Typography varient="body2" color="textSecondary">{achievement.tags.map((tag) => `#${tag} `)}</Typography>
+            </div>
+            <CardContent>
+                <Typography className={classes.title} variant="h5">
+                    {achievement.message}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <ThumbUpAltIcon fontSize="small"/>
+                    Likes {achievement.likeCount}
+
+                </Button>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <DeleteIcon fontSize="small"/>
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
     );
 };
 
--- client/src/components/Achievements/Achievements.js
+++ client/src/components/Achievements/Achievements.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import Achievement from './Achievement/Achievement.js'
+import {Grid, CircularProgress} from "@material-ui/core";
 import {useSelector} from "react-redux";
+import useStyles from "./styles"
 
 const Achievements = () => {
     const achievements = useSelector((state) => {
         return state.achievements
     })
+    const classes = useStyles();
     console.log("achievementss", achievements)
     return (
-        <>
-            <Achievement />
-            <Achievement />
-            <Achievement />
-        </>
+        !achievements.length ? <CircularProgress/>: (
+            <Grid className = {classes.mainContainer} container alignItems="stretch" spacing={3}>
+                {achievements.map((achievement) => { return(
+                    <Grid key={achievement._id} item xs={12} sm={6}>
+                        <Achievement achievement = {achievement}/>
+                    </Grid>
+                )})}
+            </Grid>
+        )
     )
 }
 
