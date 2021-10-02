56057b8269fdc65887cf2ff4800b509e9bc805a4
Mazen Sayed <54520113+Mazensayed91@users.noreply.github.com>
2021-10-02 15:03:59 +0200

    added auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+
+const auth = async (req, res, next) => {
+    try{
+        console.log("req inside auth ", req.headers)
+        const token = req.headers.authorization.split(" ")[1]
+        const isCustomAuth = token.length < 500
+
+        let decodedData;
+
+        if(token && isCustomAuth){
+            decodedData = jwt.verify(token, 'test')
+
+            req.userId = decodedData?.id;
+
+        } else{
+            decodedData = jwt.decode(token)
+
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+    } catch (e) {
+        console.log("error in auth", error)
+    }
+
+
+}
+
+export default auth;
--- server/routes/achievements.js
+++ server/routes/achievements.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import {getAchievements, createAchievement, updateAchievement, deleteAchievement, likeAchievement} from '../controllers/achievements.js'
+import auth from "../middleware/auth.js";
 
 const router = express.Router()
 
 router.get('/', getAchievements);
-router.post('/', createAchievement);
-router.patch('/:id', updateAchievement)
-router.delete('/:id', deleteAchievement)
-router.patch('/:id/likeAchievement', likeAchievement)
+router.post('/', auth, createAchievement);
+router.patch('/:id', auth, updateAchievement)
+router.delete('/:id', auth, deleteAchievement)
+router.patch('/:id/likeAchievement', auth, likeAchievement)
 
 export default router
